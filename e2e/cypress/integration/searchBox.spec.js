e468a699e0d12adcf47f751dc19742970deaca2e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2022-05-25 17:15:27 +0200

    feat: add loading test

--- e2e/cypress/integration/searchBox.spec.js
+++ e2e/cypress/integration/searchBox.spec.js
@@ -1,43 +1,82 @@
 /// <reference types="cypress"/>
+
 import repositories0 from '../fixtures/repositories_0.json';
 import repositories1000 from '../fixtures/repositories_1000.json';
 
 describe('First test', function () {
-  beforeEach(function () {
+  this.beforeEach(function () {
     cy.visit('/');
 
     cy.intercept('https://api.github.com/repositories?since=*', (req) => {
+      req.on('response', (res) => {
+        res.setDelay(500);
+      });
+
       req.reply(req.query.since === '0' ? repositories0 : repositories1000);
     }).as('api');
 
-    cy.get(':nth-child(2) > .skeleton-item').as('loading');
-    cy.get(':nth-child(2) > .result-item > article > .result-item__name').as('cardName');
+    cy.get('.search-box__input').as('input');
   });
 
-  it('Search by press enter', () => {
-    cy.get('@loading').should('be.visible');
-
-    cy.wait('@api').then(() => {
-      cy.get('@cardName').should('contain', 'wycats/merb-core');
+  it('Check search by press enter', () => {
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.match(/since=0/);
     });
 
-    cy.get('.search-box__input').type('1000{enter}');
+    cy.get('@input').type('1000{enter}');
 
-    cy.wait('@api');
-
-    cy.get('@cardName').should('contain', 'mojombo/fixture-scenarios');
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.match(/since=1000/);
+    });
   });
 
-  it('Search by click button', () => {
-    cy.get('@loading').should('be.visible');
-
-    cy.get('@cardName').should('contain', 'wycats/merb-core');
+  it('Check search by click button', () => {
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.match(/since=0/);
+    });
 
     cy.get('.search-box__input').type('1000');
     cy.get('.search-box__button').click();
 
-    cy.wait('@api');
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.match(/since=1000/);
+    });
+  });
 
-    cy.get('@cardName').should('contain', 'mojombo/fixture-scenarios');
+  it('Check loading component when searching data', () => {
+    const getLoading = (index = 1) => cy.get(`:nth-child(${index}) > .skeleton-item`);
+
+    getLoading().should('be.visible');
+
+    cy.wait('@api');
+    getLoading().should('not.exist');
+
+    cy.get('@input').type('1000{enter}');
+
+    getLoading().should('be.visible');
+
+    cy.wait('@api')
+    .then((interception) => {
+      expect(interception.request.url).to.match(/since=1000/);
+    });
+
+    getLoading().should('not.exist');
+  });
+
+  it('Check render cards', () => {
+    for (let index = 0; index < repositories0.length; index++) {
+      const card = repositories0[index];
+
+      const childIndex = index + 1;
+
+      cy.get(`:nth-child(${childIndex}) > .result-item > article > .result-item__header > .result-item__cover`)
+        .invoke('attr', 'src')
+        .should('eq', card.owner.avatar_url);
+      cy.get(`:nth-child(${childIndex}) > .result-item > article > .result-item__name`).should('contain', card.name);
+      cy.get(`:nth-child(${childIndex}) > .result-item > article > .result-item__artist-name`).should(
+        'contain',
+        card.description
+      );
+    }
   });
 });
